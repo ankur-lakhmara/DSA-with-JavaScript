cb268e2671ab28f7fadcf527977bcc21add2720d
Ankur <59440628+ankur-lakhmara@users.noreply.github.com>
2025-11-02 10:27:08 +0530

    two pointer approach

---
+++ day7/code/sumZero.js
@@ -0,0 +1,38 @@
+//brute force approach with n^2 time complexity
+let arr = [-9,-2,-1,0,1,2,3,4,5];
+// console.log(arr.sort());
+
+
+for(let i = 0;i<arr.length;i++){
+    for(let j = i+1;j<arr.length;j++){
+        if(arr[i]+arr[j] == 0){
+            console.log(`elements found at ${i} and ${j}`);
+            return;
+        }
+    }
+}
+console.log('pair not found ');
+return;
+
+//optimized approach with two pointer 
+
+let left = 0;
+let right  = arr.length-1;
+for(let i = 0;i<arr.length;i++){
+    if(arr[left]+arr[right] == 0){
+        console.log(`Elements found at ${left} and ${arr.length-right+1} indexes`);
+        return;
+    }
+    else if(arr[left]+arr[right]>0){
+        // console.log('first condition');
+        right--;
+    }
+    else{
+        // console.log('i am second condition');
+        left++;
+    }
+}
+console.log('no pair found ');
+return;
+
+
